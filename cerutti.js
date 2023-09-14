5d661d7dc4729cfb82b84e4e6d8e412fdfae46ba
Fabrizio Cerutti <fabri.cerutti@gmail.com>
2023-09-14 17:09:46 -0300

    subiendo preentrega

--- cerutti.js
+++ cerutti.js
@@ -1,48 +1,79 @@
-    //calculadora de gastos 
+//calculadora de gastos 
     
-    //mensaje de entrada
-    alert("Calculadora de gastos")
+   // Objeto que representa un gasto
+function Gasto(nombre, cantidad) {
+  this.nombre = nombre;
+  this.cantidad = cantidad;
+}
 
-    // Objeto que representa un gasto
-    function Gasto(nombre, cantidad) {
-    this.nombre = nombre;
-    this.cantidad = cantidad;
+// Array para almacenar los gastos
+let listaGastos = [];
+
+// Función para agregar un gasto a la lista y actualizar la interfaz 
+function agregarGasto() {
+  const nombreGasto = document.getElementById("nombreGasto").value;
+  const cantidadGasto = parseFloat(document.getElementById("cantidadGasto").value);
+
+  if (nombreGasto && !isNaN(cantidadGasto)) {
+      let nuevoGasto = new Gasto(nombreGasto, cantidadGasto);
+      listaGastos.push(nuevoGasto);
+
+      // Actualizar la lista de gastos y el total
+      actualizarListaGastos();
+      actualizarTotalGastos();
+
+      // Limpiar los campos de entrada
+      document.getElementById("nombreGasto").value = "";
+      document.getElementById("cantidadGasto").value = "";
   }
-  
-    // Array para almacenar los gastos
-    let listaGastos = [];
-  
-    // Función para agregar un gasto a la lista
-    function agregarGasto(nombre, cantidad) {
-    let nuevoGasto = new Gasto(nombre, cantidad);
-    listaGastos.push(nuevoGasto);
-  }
-  
-    // Función para calcular el total de gastos
-    function calcularTotalGastos() {
-        let total = 0;
-        for (let gasto of listaGastos) {
+}
+
+// Función para calcular el total de gastos
+function calcularTotalGastos() {
+  let total = 0;
+  for (let gasto of listaGastos) {
       total += gasto.cantidad;
-    }
-    return total;
   }
-  
-    // El usuario ingresa el nombre del gasto y la cantidad del gasto
-    while (true) {
-    let nombreGasto = prompt("Ingrese el nombre del gasto (o 'fin' para finalizar):");
-    if (nombreGasto.toLowerCase() === "fin") {
-      break;
-    }
-    let cantidadGasto = parseFloat(prompt("Ingrese la cantidad del gasto:"));
-    agregarGasto(nombreGasto, cantidadGasto);
+  return total;
+}
+
+// Función para actualizar la lista de gastos en la interfaz 
+function actualizarListaGastos() {
+  const listaGastosElement = document.getElementById("listaGastos");
+  listaGastosElement.innerHTML = "";
+  for (let gasto of listaGastos) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${gasto.nombre}: $${gasto.cantidad.toFixed(2)}`;
+      listaGastosElement.appendChild(listItem);
   }
-  
-     // Calcular el total de gastos
-    let totalGastos = calcularTotalGastos();
-  
+}
 
-    alert(`Total de gastos: $${totalGastos}`); 
+// Función para actualizar el total de gastos en la interfaz 
+function actualizarTotalGastos() {
+  const totalGastosElement = document.getElementById("totalGastos");
+  totalGastosElement.textContent = calcularTotalGastos().toFixed(2);
+}
 
-    console.log(`Total de gastos: $${totalGastos}`);
+// Manejar el clic en el botón "Agregar Gasto"
+const agregarGastoButton = document.getElementById("agregarGasto");
+agregarGastoButton.addEventListener("click", agregarGasto);
 
+// Cargar gastos almacenados en localStorage al cargar la página
+if (localStorage.getItem("gastos")) {
+  listaGastos = JSON.parse(localStorage.getItem("gastos"));
+  actualizarListaGastos();
+  actualizarTotalGastos();
+}
 
+// Guardar gastos en localStorage cuando se agrega uno nuevo
+function guardarGastosEnLocalStorage() {
+  localStorage.setItem("gastos", JSON.stringify(listaGastos));
+}
+
+// Actualizar gastos en localStorage cuando se agrega uno nuevo
+agregarGastoButton.addEventListener("click", guardarGastosEnLocalStorage);
+
+// Borrar datos de localStorage cuando la página se refresca
+window.addEventListener("beforeunload", function() {
+  localStorage.clear();
+});
